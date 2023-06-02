cbe39ed0f1383005939e93242a9a1e50672f4cac
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-02 11:18:25 +0000

    products now appear in a grid on product page

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Photos
 
 # Create your views here.
+def all_photos(request):
+
+    photos = Photos.objects.all()
+
+    context = {
+        'photos': photos,
+    }
+    return render(request, 'photos/photos.html', context)
