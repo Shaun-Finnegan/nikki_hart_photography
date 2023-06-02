2b9f76db540f001722850a7a8381a34281d856ca
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-02 10:37:51 +0000

    admin is now customized

--- photos/admin.py
+++ photos/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Photos, Category
 
 # Register your models here.
-admin.site.register(Photos)
-admin.site.register(Category)
+class PhotosAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Photos, PhotosAdmin)
+admin.site.register(Category, CategoryAdmin)
--- photos/models.py
+++ photos/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +16,9 @@ class Category(models.Model):
 
 
 class Photos(models.Model):
+    class Meta:
+        verbose_name_plural = 'Photos'
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
