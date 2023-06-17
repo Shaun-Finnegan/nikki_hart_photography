5e96e432a9d510d367f093083e0fd108fe5dccac
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-17 12:05:36 +0000

    shopping bag now reflects what has been added by the customer

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from photos.models import Photos
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        photo = get_object_or_404(Photos, pk=item_id)
+        total += quantity * photo.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'photo': photo,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
