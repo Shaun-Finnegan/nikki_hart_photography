a5193541fbcf89e696231913c1b035cf602cb0bc
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-18 13:47:11 +0000

    can now adjust and remove items from shopping bag, however the quantity doesn't show in the form, need to address this

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,18 +7,30 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    photo_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        photo = get_object_or_404(Photos, pk=item_id)
-        total += quantity * photo.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'photo': photo,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            photo = get_object_or_404(Photos, pk=item_id)
+            total += item_data * photo.price
+            photo_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'photo': photo,
+            })
+        else:
+            photo = get_object_or_404(Photos, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * photo.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'photo': photo,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -32,7 +44,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'photo_count': photo_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
