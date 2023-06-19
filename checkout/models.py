1b302d307dc6882b6b47f42f652d124321f5a626
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-19 12:15:20 +0000

    payment functionality now in place and when order is placed it is correctly updated in the database and redirects the user to a thank you screen

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -67,8 +67,8 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.photos.price * self.quantity
+        self.lineitem_total = self.photo.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.photos.sku} on order {self.order.order_number}'
+        return f'SKU {self.photo.sku} on order {self.order.order_number}'
