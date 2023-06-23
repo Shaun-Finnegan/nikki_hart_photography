be7b01c8ae64357bf62a07575f87a82066fabd75
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-23 14:41:48 +0000

    adjusted allowed hosts

--- nikki_hart_photography/settings.py
+++ nikki_hart_photography/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-)#ir#km4vu4+8h(&oq36fp5^f5_&ca))rvv3u#kfwo9ztzpxav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['nikki-hart-photography-5b89f5a6c233.herokuapp.com'. You may need to add 'nikki-hart-photography-5b89f5a6c233.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-shaunfinneg-nikkihartph-9tm58irb827.ws-eu100.gitpod.io']
 
 
 # Application definition
