cd8ef47d263ba0dc1dcdaf4730034b965a32a4b0
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-30 10:43:16 +0000

    removed secret key

--- nikki_hart_photography/settings.py
+++ nikki_hart_photography/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)#ir#km4vu4+8h(&oq36fp5^f5_&ca))rvv3u#kfwo9ztzpxav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-shaunfinneg-nikkihartph-9tm58irb827.ws-eu101.gitpod.io', 'nikki-hart-photography-5b89f5a6c233.herokuapp.com']
 
