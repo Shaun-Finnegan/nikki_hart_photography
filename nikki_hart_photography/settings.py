9f35012ad477a5d4db78712259495c45b11988a1
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-06-30 10:23:56 +0000

    changed secret key

--- nikki_hart_photography/settings.py
+++ nikki_hart_photography/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-)#ir#km4vu4+8h(&oq36fp5^f5_&ca))rvv3u#kfwo9ztzpxav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
